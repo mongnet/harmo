8f42e8355ae55a302b5bb15e57402662f69d3903
mongnet <250021520@qq.com>
2024-07-22 16:47:28 +0800

    1.录制脚本优化，减少冗余代码

--- harmo/mitm/addons.py
+++ harmo/mitm/addons.py
@@ -21,6 +21,11 @@ class Counter:
         self.filterFiles = Global_Map.get("Setting").get("filterFile")
 
     def request(self, flow: http.HTTPFlow):
+        """
+        请求拦截
+        :param flow:
+        :return:
+        """
         # 记录请求信息
         if self.match(flow):
             flow.customField = {}
@@ -31,13 +36,18 @@ class Counter:
             flow.customField.update({"headers": dict(flow.request.headers)})
             flow.customField.update({"body": flow.request.get_text()})
 
-    def match(self, flow):
+    def match(self, flow) -> bool:
+        """
+        判断是否需要录制
+        :param flow:
+        :return:
+        """
         if not self.domains:
             ctx.log.error("必须配置允许录制的域名列表")
             exit(-1)
         # 跳过不录制的请求方法,如：options
         if isinstance(Global_Map.get("Setting").get("filterMethod"), list) and flow.request.method.lower() in Global_Map.get("Setting").get("filterMethod"):
-            return
+            return False
         # 过滤不录制的域名和请求
         for domain in self.domains:
             parsed_url = urlparse(flow.request.url)
@@ -50,6 +60,11 @@ class Counter:
         return False
 
     def response(self, flow):
+        """
+        响应拦截
+        :param flow:
+        :return:
+        """
         if self.match(flow):
             flow.end_time = time.time()
             try:
@@ -67,45 +82,47 @@ class Counter:
                 with open('record_results.json','w',encoding='utf-8') as f:
                     f.write(json.dumps(self.interfaces, ensure_ascii=False))
 
-    def clean_data(self,interface):
+    def clean_data(self,interface) -> dict:
+        """
+        清洗数据
+        :param interface:
+        :return:
+        """
+        data_json = {}
         if isinstance(interface,dict):
-            data_json={}
-            if not interface.get("url").endswith("/"):
-                data_json['id'] = base_utils.generate_random_str(randomlength=10)
-                data_json['url'] = interface.get("url")
-                data_json['path'] = interface.get("path")
-                data_json['method'] = interface.get("method")
-                data_json['headers'] = interface.get("headers")
-                try:
-                    if isinstance(interface.get("body"),(dict,list)):
-                        base_utils.recursion_replace_dict_value(interface.get("body"), Global_Map.get("Setting").get("replaceDict"))
-                        data_json['body'] = interface.get("body")
-                    elif 'WebKitFormBoundary' not in str(interface.get("body")):
-                        if interface.get("body") !='' :
-                            data_json['body'] = eval(str(interface.get("body")).replace('null','None').replace('true','True').replace('false','False'))
-                            # data_json['body'] = interface.get("body")
-                        else:
-                            data_json['body'] = None
+            data_json['id'] = base_utils.generate_random_str(randomlength=10)
+            data_json['url'] = interface.get("url")
+            data_json['path'] = interface.get("path")
+            data_json['method'] = interface.get("method")
+            data_json['headers'] = interface.get("headers")
+            try:
+                if isinstance(interface.get("body"),(dict,list)):
+                    base_utils.recursion_replace_dict_value(interface.get("body"), Global_Map.get("Setting").get("replaceDict"))
+                    data_json['body'] = interface.get("body")
+                elif 'WebKitFormBoundary' not in str(interface.get("body")):
+                    if interface.get("body") !='' :
+                        data_json['body'] = eval(str(interface.get("body")).replace('null','None').replace('true','True').replace('false','False'))
+                        # data_json['body'] = interface.get("body")
                     else:
-                        body = str(interface.get("body"))
-                        bodylist = body.split('\r\n')
-                        data_json['body']={}
-                        for each in bodylist:
-                            if 'name=' in each:
-                                if 'file' not in str(each) and 'attachment' not in str(each):
-                                    data_json['body'][each.split('\"')[1]] = bodylist[int(int(bodylist.index(each))+2)]
-                                else:
-                                    data_json['body']['doc'] = each.split('\"')[1]
-                except:
-                    data_json['body'] = None
-                try:
-                    base_utils.recursion_replace_dict_value(interface.get("resp"), Global_Map.get("Setting").get("replaceDict"))
-                    data_json['resp'] = interface.get("resp")
-                except:
-                    data_json['resp'] = None
-            if data_json:
-                return data_json
-        return
+                        data_json['body'] = None
+                else:
+                    body = str(interface.get("body"))
+                    bodylist = body.split('\r\n')
+                    data_json['body']={}
+                    for each in bodylist:
+                        if 'name=' in each:
+                            if 'file' not in str(each) and 'attachment' not in str(each):
+                                data_json['body'][each.split('\"')[1]] = bodylist[int(int(bodylist.index(each))+2)]
+                            else:
+                                data_json['body']['doc'] = each.split('\"')[1]
+            except:
+                data_json['body'] = None
+            try:
+                base_utils.recursion_replace_dict_value(interface.get("resp"), Global_Map.get("Setting").get("replaceDict"))
+                data_json['resp'] = interface.get("resp")
+            except:
+                data_json['resp'] = None
+        return data_json
 
 def getAllowRecording() -> list:
     """
@@ -115,7 +132,7 @@ def getAllowRecording() -> list:
     AllowRecording = Global_Map.get("Setting").get("allowRecording")
     if isinstance(AllowRecording, list) and AllowRecording:
         return AllowRecording
-    raise TypeError("允许录制的域名列表为空，请确认 config.yaml 中 Setting.allowRecording 是否配置")
+    raise ValueError("允许录制的域名列表为空，请确认 config.yaml 中 Setting.allowRecording 是否配置")
 
 addons = [
     Counter(
