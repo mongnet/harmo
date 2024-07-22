5b0b672fe5a642b129e0277dc925877238b77b36
mongnet <250021520@qq.com>
2024-07-22 17:46:11 +0800

    1.录制脚本优化，减少冗余代码

--- harmo/mitm/record.py
+++ harmo/mitm/record.py
@@ -12,20 +12,20 @@ def run_mitmdump_script():
     command = ['mitmdump', '-s', addons_py_path]
     print("录制已开始，请设置好代理并开始操作，按 ctrl+C 结束录制.")
     # 使用 subprocess.Popen 来启动命令并获取输出
-    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, env=os.environ, encoding="utf-8") as process:
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, env=os.environ) as process:
         try:
             # 实时获取并打印 mitmdump 的输出
             for line in process.stdout:
-                print(line, end='')  # 使用 end='' 避免每次打印后出现额外的换行符
+                # 使用 end='' 避免每次打印后出现额外的换行符
+                print(line, end='')
         except KeyboardInterrupt:
-            # 如果需要，可以在这里处理 KeyboardInterrupt 异常
-            # 例如，可以通过 process.terminate() 终止进程
+            # 通过 process.terminate() 终止进程
             process.terminate()
             print("录制被手动终止.")
+        except Exception as e:
+            process.terminate()
+            print(f"录制异常终止: {e}")
         finally:
-            # 等待进程结束并获取退出码
-            # process.wait()
-            # exit_code = process.returncode
             print(f"录制结束.")
 
 if __name__ == '__main__':
